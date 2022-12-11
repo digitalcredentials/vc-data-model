b1e4b345801e4fb4876c2eb7f6fd77e4bb81db94
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-12-11 15:15:12 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: ['standard-with-typescript', 'prettier', 'eslint-config-prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  ignorePatterns: ['dist/', 'test'],
+  rules: {
+    'prettier/prettier': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    '@typescript-eslint/return-await': 'off'
+  }
+}

b7779c212cf9a572aebf7a93d395d4d88f6f352f
Kayode Ezike <kezike13@gmail.com>
2023-02-20 23:55:26 -0500

    fixed lint error by adding types folder to ignorePatterns array

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json'
   },
-  ignorePatterns: ['dist/', 'test'],
+  ignorePatterns: ['dist', 'test', 'types'],
   rules: {
     'prettier/prettier': 'off',
     'arrow-body-style': 'off',
